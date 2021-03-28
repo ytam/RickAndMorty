a16b6b805e840bc0a754e827d433fab778921843
Yıldırım Tam <yildirimtam@gmail.com>
2021-03-28 23:13:50 +0300

    add image anim

--- app/src/main/java/io/github/ytam/rickandmorty/extensions/LoadImage.kt
+++ app/src/main/java/io/github/ytam/rickandmorty/extensions/LoadImage.kt
@@ -3,6 +3,9 @@ package io.github.ytam.rickandmorty.extensions
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import io.github.ytam.rickandmorty.R
 
 /**
@@ -10,10 +13,22 @@ import io.github.ytam.rickandmorty.R
  */
 fun ImageView.loadFromUrl(url: String) {
 
+    var options = RequestOptions()
+        .centerCrop()
+        .placeholder(R.drawable.progress_animation)
+        .error(R.mipmap.ic_launcher_round)
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
+        .priority(Priority.HIGH)
+        .dontAnimate()
+        .dontTransform();
+
+
     Glide.with(context)
+        .setDefaultRequestOptions(options)
         .load(url)
         .placeholder(R.mipmap.ic_launcher)
         .error(R.mipmap.ic_launcher_round)
+        .apply(options)
         .into(this)
 }
 
